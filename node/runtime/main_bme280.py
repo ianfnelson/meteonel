3c2e0cc5744b12e2e99f57a916ec960b0e28475f
Ian Nelson <ian@iannelson.systems>
2020-01-18 20:13:20 +0000

    Add pika to lite setup. Tweaking main_bme280

--- node/runtime/main_bme280.py
+++ node/runtime/main_bme280.py
@@ -2,22 +2,11 @@ import datetime
 import time
 import sys
 import json
-import iothub_client
+import pika
 import sensor_bme280
 
-# TODO - move this stuff to settings class
-CONNECTION_STRING = "HostName=meteonel-hub-dev.azure-devices.net;DeviceId=office1;SharedAccessKey=ZC6ewYTaqGhLZDfNaUC9qSOsfw7fXZ0RTx2vtjNOPzU="
-PROTOCOL = IoTHubTransportProvider.MQTT
-MESSAGE_TIMEOUT = 10000
-DEVICE_ID = "office1"
-
-def send_confirmation_callback(message, result, user_context):
-    print ( "IoT Hub responded to message with status: %s" % (result) )
-
-def iothub_client_init():
-    # Create an IoT Hub client
-    client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
-    return client
+DEVICE_ID = "leekpi"
+VHOST = "meteonel_dev"
 
 def bme280_node_run():
 
@@ -27,29 +16,18 @@ def bme280_node_run():
 
             humidity, pressure, temperature = bme280_sensor.read_all()
 
-            # TODO - optionally read ds18b20 data
-
             data = {}
+            data['device'] = DEVICE_ID
             data['timestamp'] = datetime.datetime.utcnow().isoformat() + 'Z'
-            data['device_id'] = DEVICE_ID
-            data['sensor_type'] = 'temperature'
-            data['temperature_ambient_celsius'] = temperature
-            data['humidity_relative_percentage'] = humidity
-            data['pressure_millibars'] = pressure
+            data['tempAmbient'] = temperature
+            data['humidity'] = humidity
+            data['pressure'] = pressure
 
             json_data = json.dumps(data)
-                        
-            message = IoTHubMessage(json_data)
 
             # Send the message.
             print( "Sending message: %s" % message.get_string() )
-            client.send_event_async(message, send_confirmation_callback, None)
-            
-            time.sleep(4.9)
 
-    except IoTHubError as iothub_error:
-        print ( "Unexpected error %s from IoTHub" % iothub_error )
-        return
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
 
--- node/setup/setup_lite.sh
+++ node/setup/setup_lite.sh
@@ -5,3 +5,4 @@ sudo apt-get install i2c-tools
 
 pip3 install RPi.bme280
 pip3 install smbus2
+pip3 install pika
